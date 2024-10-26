fade82aab9e0986efe0a573751c61efd7f0fa642
pranjal23030 <pranjal.khatiwoda@gmail.com>
2024-10-26 12:08:33 +0545

    verify otp and reset password route

--- src/routes/userRoute.ts
+++ src/routes/userRoute.ts
@@ -8,5 +8,7 @@ const router = express.Router()
 router.route("/register").post(UserController.register)
 router.route("/login").post(UserController.login)
 router.route("/forgot-password").post(UserController.handleForgotPassword)
+router.route("/verify-otp").post(UserController.verifyOTP)
+router.route("/reset-password").post(UserController.resetPassword)
 
 export default router
