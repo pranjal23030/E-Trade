b15672b6e0491a106b701267fac83833a26cc4f5
pranjal23030 <pranjal.khatiwoda@gmail.com>
2024-11-14 20:50:20 +0545

    category seeder

--- server.ts
+++ server.ts
@@ -1,10 +1,12 @@
 import adminSeeder from "./adminSeeder";
 import app from "./src/app";
 import { envConfig } from "./src/config/config";
+import categoryController from "./src/controllers/categoryController";
 
 function startServer() {
     const port = envConfig.port || 4000
     app.listen(envConfig.port, () => {
+        categoryController.seedCategory()
         console.log(`Server has started at port [${port}]`)
         adminSeeder()
     })
