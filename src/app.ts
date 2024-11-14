8fc238b1f0885a8a9554b8cb737bb3e36db77331
pranjal23030 <pranjal.khatiwoda@gmail.com>
2024-11-14 20:51:25 +0545

    cartegory route

--- src/app.ts
+++ src/app.ts
@@ -1,14 +1,18 @@
 import express from 'express'
 import './database/connection'
 import userRoute from './routes/userRoute'
+import categoryRoute from './routes/categoryRoute'
+import User from './database/models/userModel'
 
 const app = express()
 
 
 app.use(express.json())
 
+
 // localhost:3000/api/auth/---
 app.use("/api/auth", userRoute)
+app.use("/api/category", categoryRoute)
 
 
 
