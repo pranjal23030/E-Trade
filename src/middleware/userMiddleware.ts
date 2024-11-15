ee2f492fd2d11373e40c55f6acf04b3086b9ff33
pranjal23030 <pranjal.khatiwoda@gmail.com>
2024-11-15 21:33:05 +0545

    restrict customer to add category

--- src/middleware/userMiddleware.ts
+++ src/middleware/userMiddleware.ts
@@ -2,12 +2,28 @@ import { NextFunction, Request, Response } from "express";
 import jwt from 'jsonwebtoken'
 import { envConfig } from "../config/config";
 import UserController from "../controllers/userController";
+import User from "../database/models/userModel";
 
+export enum Role {
+    Admin = 'admin',
+    Customer = 'customer'
+}
+
+// adding user to Request type
+interface IExtendedRequest extends Request {
+    user?: {
+        username: string,
+        email: string,
+        role: string,
+        password: string
+        id: string
+    }
+}
 
 class UserMiddleware {
-    async isUserLoggedIn(req: Request, res: Response, next: NextFunction): Promise<void> {
+    async isUserLoggedIn(req: IExtendedRequest, res: Response, next: NextFunction): Promise<void> {
         // receive token 
-        const token = req.headers.authorization 
+        const token = req.headers.authorization
         if (!token) {
             res.status(403).json({
                 message: "Token must be provided."
@@ -15,18 +31,30 @@ class UserMiddleware {
             return
         }
         // validate token 
-        jwt.verify(token, envConfig.jwtSecretKey as string, async (err, result) => {
+        jwt.verify(token, envConfig.jwtSecretKey as string, async (err, result: any) => {
             if (err) {
                 res.status(403).json({
                     message: "Invalid token !!!"
                 })
             } else {
                 console.log(result) //{userId : 123123123}
-                // @ts-ignore
-                req.userId = result.userId
+                const userData = await User.findByPk(result?.userId) // {email, pass, role}
+                if (!userData) {
+                    res.status(404).json({
+                        mesasge: "No user with that userId."
+                    })
+                    return
+                }
+                req.user = userData
                 next()
             }
         })
+    }
+    restrictTo(...roles: Role[]) {  // admin or customer only
+        return (req: IExtendedRequest, res: Response, next: NextFunction) => {
+            let userRole = req.user?.role as Role
+            console.log(userRole,"Role")
+        }
     }
 }
 
