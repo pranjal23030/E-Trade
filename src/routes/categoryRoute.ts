ca5f5320853046e5f61652cf33752f215b8db3e5
pranjal23030 <pranjal.khatiwoda@gmail.com>
2024-11-17 21:47:26 +0545

    middleware added

--- src/routes/categoryRoute.ts
+++ src/routes/categoryRoute.ts
@@ -3,7 +3,7 @@ import categoryController from '../controllers/categoryController'
 import userMiddleware, { Role } from '../middleware/userMiddleware'
 const router: Router = express.Router()
 
-router.route("/").get(categoryController.getCategories).post(userMiddleware.isUserLoggedIn, userMiddleware.restrictTo(Role.Admin), categoryController.addCategory)
-router.route("/:id").patch(categoryController.updateCategory).delete(categoryController.deleteCategory)
+router.route("/").get(categoryController.getCategories).post(userMiddleware.isUserLoggedIn, userMiddleware.accessTo(Role.Admin), categoryController.addCategory)
+router.route("/:id").patch(userMiddleware.isUserLoggedIn, userMiddleware.accessTo(Role.Admin), categoryController.updateCategory).delete(userMiddleware.isUserLoggedIn, userMiddleware.accessTo(Role.Admin), categoryController.deleteCategory)
 
 export default router 
