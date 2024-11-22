08b849566d63b40224e38b074dacea6b7a2c0d56
pranjal23030 <pranjal.khatiwoda@gmail.com>
2024-11-22 20:21:50 +0545

    error handle product route

--- src/routes/productRoute.ts
+++ src/routes/productRoute.ts
@@ -3,12 +3,18 @@ import productController from '../controllers/productController'
 import userMiddleware, { Role } from '../middleware/userMiddleware'
 
 import { multer, storage } from '../middleware/multerMiddleware'
+import errorHandler from '../services/errorHandler'
 const upload = multer({ storage: storage })
 
 const router: Router = express.Router()
 
-router.route("/").post(userMiddleware.isUserLoggedIn, userMiddleware.accessTo(Role.Admin), upload.single("productImage"), productController.createProduct).get(productController.getAllProducts)
-router.route("/:id").delete(userMiddleware.isUserLoggedIn, userMiddleware.accessTo(Role.Admin), productController.deleteProduct).get(productController.getSingleProduct)
+router.route("/").
+    post(userMiddleware.isUserLoggedIn, userMiddleware.accessTo(Role.Admin), upload.single("productImage"), errorHandler(productController.createProduct))
+    .get(errorHandler(productController.getAllProducts))
+
+router.route("/:id")
+    .delete(userMiddleware.isUserLoggedIn, userMiddleware.accessTo(Role.Admin), errorHandler(productController.deleteProduct))
+    .get(errorHandler(productController.getSingleProduct))
 
 
 export default router
