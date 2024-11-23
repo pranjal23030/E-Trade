274f35600838940daae4fe7ca2b0afe69eb506e8
pranjal23030 <pranjal.khatiwoda@gmail.com>
2024-11-23 11:21:03 +0545

    error handler category route

--- src/routes/categoryRoute.ts
+++ src/routes/categoryRoute.ts
@@ -1,9 +1,15 @@
 import express, { Router } from 'express'
 import categoryController from '../controllers/categoryController'
 import userMiddleware, { Role } from '../middleware/userMiddleware'
+import errorHandler from '../services/errorHandler'
 const router: Router = express.Router()
 
-router.route("/").get(categoryController.getCategories).post(userMiddleware.isUserLoggedIn, userMiddleware.accessTo(Role.Admin), categoryController.addCategory)
-router.route("/:id").patch(userMiddleware.isUserLoggedIn, userMiddleware.accessTo(Role.Admin), categoryController.updateCategory).delete(userMiddleware.isUserLoggedIn, userMiddleware.accessTo(Role.Admin), categoryController.deleteCategory)
+router.route("/")
+    .get(errorHandler(categoryController.getCategories))
+    .post(userMiddleware.isUserLoggedIn, userMiddleware.accessTo(Role.Admin), errorHandler(categoryController.addCategory))
+
+router.route("/:id")
+    .patch(userMiddleware.isUserLoggedIn, userMiddleware.accessTo(Role.Admin), errorHandler(categoryController.updateCategory))
+    .delete(userMiddleware.isUserLoggedIn, userMiddleware.accessTo(Role.Admin), errorHandler(categoryController.deleteCategory))
 
 export default router 
