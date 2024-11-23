70d66979b39c354aa2964740e1b1acc675876ca5
pranjal23030 <pranjal.khatiwoda@gmail.com>
2024-11-23 11:20:09 +0545

    error handler user route

--- src/routes/userRoute.ts
+++ src/routes/userRoute.ts
@@ -1,14 +1,15 @@
 import express from 'express'
 import UserController from '../controllers/userController'
+import errorHandler from '../services/errorHandler'
 const router = express.Router()
 
 // router.post("/register",UserController.register)
 // router.get("/register",UserController.register)
 
-router.route("/register").post(UserController.register)
-router.route("/login").post(UserController.login)
-router.route("/forgot-password").post(UserController.handleForgotPassword)
-router.route("/verify-otp").post(UserController.verifyOTP)
-router.route("/reset-password").post(UserController.resetPassword)
+router.route("/register").post(errorHandler(UserController.register))
+router.route("/login").post(errorHandler(UserController.login))
+router.route("/forgot-password").post(errorHandler(UserController.handleForgotPassword))
+router.route("/verify-otp").post(errorHandler(UserController.verifyOTP))
+router.route("/reset-password").post(errorHandler(UserController.resetPassword))
 
 export default router
