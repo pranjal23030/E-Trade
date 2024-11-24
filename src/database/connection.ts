04a9339b03babe277876591308f92ebd46023356
pranjal23030 <pranjal.khatiwoda@gmail.com>
2024-11-24 22:08:53 +0545

    relationships

--- src/database/connection.ts
+++ src/database/connection.ts
@@ -1,5 +1,11 @@
 import { Sequelize } from "sequelize-typescript";
 import { envConfig } from "../config/config";
+import Product from "./models/productModel";
+import Category from "./models/categoryModel";
+import User from "./models/userModel";
+import Order from "./models/orderModel";
+import Payment from "./models/paymentModel";
+import OrderDetails from "./models/orderDetails";
 
 const sequelize = new Sequelize(envConfig.connectionString as string, {
     models: [__dirname + '/models']
@@ -20,5 +26,33 @@ try {
 sequelize.sync({ force: false, alter: false }).then(() => {
     console.log("Changes added to database. 👍👍")
 })
+
+// Relationships
+
+// any product belongs to a category and a category has a product
+// categoryId in product table
+
+Product.belongsTo(Category, { foreignKey: 'categoryId' })
+Category.hasOne(Product, { foreignKey: 'categoryId' })
+
+// user can order many thngs and the order belongs to the user, kunchai user le kun order gareko
+// userId in order table
+
+User.hasMany(Order, { foreignKey: 'userId' })
+Order.belongsTo(User, { foreignKey: 'userId' })
+
+// an order has payment
+// orderId in payments table
+
+Payment.belongsTo(Order, { foreignKey: 'orderId' })
+Order.hasOne(Payment, { foreignKey: 'orderId' })
+
+// orderId in orderDetails
+OrderDetails.belongsTo(Order, { foreignKey: 'orderId' })
+Order.hasOne(OrderDetails, { foreignKey: 'orderId' })
+
+// productId in orderDetails
+OrderDetails.belongsTo(Product, { foreignKey: 'productId' })
+Product.hasMany(OrderDetails, { foreignKey: 'productId' })
 
 export default sequelize
