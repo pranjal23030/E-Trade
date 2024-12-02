498bb477d4de9e27d05df119009ac6b714217696
pranjal23030 <pranjal.khatiwoda@gmail.com>
2024-12-02 21:18:49 +0545

    verify pidx route

--- src/routes/orderRoute.ts
+++ src/routes/orderRoute.ts
@@ -4,9 +4,8 @@ import userMiddleware from '../middleware/userMiddleware'
 import errorHandler from '../services/errorHandler'
 const router: Router = express.Router()
 
-router.route("/")
-    .post(userMiddleware.isUserLoggedIn, errorHandler(orderController.createOrder))
-
+router.route("/").post(userMiddleware.isUserLoggedIn, errorHandler(orderController.createOrder))
+router.route("/verify-pidx").post(userMiddleware.isUserLoggedIn, errorHandler(orderController.verifyTransaction))
 
 
 export default router
