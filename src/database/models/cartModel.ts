7853f8dd234715900da2e317fbfae3423a0263f4
pranjal23030 <pranjal.khatiwoda@gmail.com>
2024-12-18 21:42:36 +0545

    cart table

---
+++ src/database/models/cartModel.ts
@@ -0,0 +1,27 @@
+// which user(fk), what product(fk) and quantity
+
+
+import { Table, Column, Model, DataType } from 'sequelize-typescript'
+
+@Table({
+    tableName: "carts",
+    modelName: "Cart",
+    timestamps: true
+})
+
+class Cart extends Model {
+    @Column({
+        primaryKey: true,
+        type: DataType.UUID,
+        defaultValue: DataType.UUIDV4
+    })
+
+    @Column({
+        type: DataType.INTEGER,
+        allowNull: false
+    })
+    declare quantity: number
+}
+
+export default Cart
+
