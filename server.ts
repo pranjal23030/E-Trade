3ac1c28cd24321fb45c67dec3904e5f2fd5723b6
pranjal23030 <pranjal.khatiwoda@gmail.com>
2024-12-20 21:52:36 +0545

    user online check websocket

--- server.ts
+++ server.ts
@@ -2,13 +2,48 @@ import adminSeeder from "./adminSeeder";
 import app from "./src/app";
 import { envConfig } from "./src/config/config";
 import categoryController from "./src/controllers/categoryController";
+import { Server } from 'socket.io'
+import jwt from 'jsonwebtoken'
+import User from "./src/database/models/userModel";
 
 function startServer() {
     const port = envConfig.port || 4000
-    app.listen(envConfig.port, () => {
+    const server = app.listen(envConfig.port, () => {
         categoryController.seedCategory()
         console.log(`Server has started at port [${port}]`)
         adminSeeder()
+    })
+    
+    // WebSocket integration 
+    const io = new Server(server, {
+        cors: {
+            origin: 'http://localhost:5173'
+        }
+    })
+    let onlineUsers: { socketId: string, userId: string, role: string }[] = []
+    let addToOnlineUsers = (socketId: string, userId: string, role: string) => {
+        onlineUsers = onlineUsers.filter((user) => user.userId !== userId)
+        onlineUsers.push({ socketId, userId, role })
+    }
+    io.on("connection", (socket) => {
+        const { token } = socket.handshake.auth // jwt token 
+        if (token) {
+            jwt.verify(token, envConfig.jwtSecretKey as string, async (err: any, result: any) => {
+                if (err) {
+                    socket.emit("error", err)
+                } else {
+                    const userData = await User.findByPk(result.userId) // {email:"",pass:"",role:""}
+                    if (!userData) {
+                        socket.emit("error", "No user found with that token")
+                        return
+                    }
+                    // userID grab
+                    // 2, 2, customer
+                    addToOnlineUsers(socket.id, result.userId, userData.role)
+                }
+            })
+
+        }
     })
 }
 
